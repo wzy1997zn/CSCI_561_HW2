735d6e155a98c85f3ace3d951feeddfe55088e6f
Ziyue Wang <u6891105@anu.edu.au>
2020-03-05 21:03:51 -0800

    first naive edition of qlearn. use board as state. terrible huge state space

---
+++ asnlib/public/dev/learning_host.py
@@ -0,0 +1,69 @@
+from copy import deepcopy
+
+import my_player3_0_1
+import host
+
+
+def learn_loop():
+    my_player_ = 1
+    last_board_ = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+    cur_board_ = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+
+    black_player = my_player3_0_1.QLearner()
+    white_player = my_player3_0_1.QLearner()
+
+    host_GO = host.GO(5)
+
+    black_state_action_list = []
+    white_state_action_list = []
+
+    for i in range(24):
+
+        # black
+        go = my_player3_0_1.Go(my_player_, last_board_, cur_board_)
+
+        black_player.set_go(go)
+        black_res = black_player.get_move()
+
+        if black_res == "PASS":
+            print("black pass")
+            break
+
+        host_GO.set_board(1, last_board_, cur_board_)
+        last_board_ = deepcopy(cur_board_)
+        host_GO.place_chess(black_res[0], black_res[1], 1)
+        host_GO.died_pieces = host_GO.remove_died_pieces(2)
+
+        cur_board_ = host_GO.board
+        black_state_action_list.append((last_board_, black_res, deepcopy(cur_board_)))
+        host_GO.visualize_board()
+
+        # white
+        go = my_player3_0_1.Go(2, last_board_, cur_board_)
+
+        white_player.set_go(go)
+        white_res = white_player.get_move()
+
+        if white_res == "PASS":
+            print("white pass")
+            break
+
+        host_GO.set_board(2, last_board_, cur_board_)
+        last_board_ = deepcopy(cur_board_)
+        host_GO.place_chess(white_res[0], white_res[1], 2)
+        host_GO.died_pieces = host_GO.remove_died_pieces(1)
+
+        cur_board_ = host_GO.board
+        white_state_action_list.append((last_board_, white_res, deepcopy(cur_board_)))
+        host_GO.visualize_board()
+
+    black_player.learn(black_state_action_list)
+    white_player.learn(white_state_action_list)
+
+
+for i in range(10000):
+    learn_loop()
+
+
+
+
--- asnlib/public/dev/my_player3_0_1.py
+++ asnlib/public/dev/my_player3_0_1.py
@@ -1,6 +1,8 @@
 # 6853115445: Ziyue Wang
 
 from copy import deepcopy
+from random import *
+import os
 
 """
 constants
@@ -10,7 +12,7 @@ INPUT = "input.txt"
 OUTPUT = "output.txt"
 EMPTY = 0
 BLACK = 1
-WRITE = 2
+WHITE = 2
 
 
 class Go:
@@ -42,7 +44,7 @@ class Go:
             for j in range(BOARD_SIZE):  # for all positions
                 if self.cur_board[i][j] == EMPTY:  # can place
                     self.test_board = deepcopy(self.cur_board)
-                    self.test_board[i][j] = self.my_player # place new stone
+                    self.test_board[i][j] = self.my_player  # place new stone
                     cur_place = (i, j)
                     if self.check_if_has_liberty(cur_place):  # satisfy 1
                         move_list.append(cur_place)
@@ -107,16 +109,16 @@ class Go:
         """
         neighbors = []
         if place[0] > 0:
-            neighbors.append((place[0]-1, place[1]))
-        if place[0] < BOARD_SIZE-1:
-            neighbors.append((place[0]+1, place[1]))
+            neighbors.append((place[0] - 1, place[1]))
+        if place[0] < BOARD_SIZE - 1:
+            neighbors.append((place[0] + 1, place[1]))
         if place[1] > 0:
-            neighbors.append((place[0], place[1]-1))
-        if place[1] < BOARD_SIZE-1:
-            neighbors.append((place[0], place[1]+1))
+            neighbors.append((place[0], place[1] - 1))
+        if place[1] < BOARD_SIZE - 1:
+            neighbors.append((place[0], place[1] + 1))
         return neighbors
 
-    def get_liberty(self, place=(-1,-1)):
+    def get_liberty(self, place=(-1, -1)):
         """
         get liberties of cur place stone
         :param place: cur stone
@@ -177,12 +179,164 @@ class RandomPlayer:
     def get_move(self):
         if len(self.move_list) == 0:
             return "PASS"
-        from random import choice
+
         return choice(self.move_list)
 
 
+import numpy as np
+
+
+class QLearner:
+    """
+    Qlearner for Little-GO
+    Q(state_i, action_i) <-- (1-alpha)*Q(state_i, action_i) + alpha * (R_state_i_action_i + gamma * max_action_Q(state_i+1, action))
+    state--     board
+    action--    move
+    R--         value deviation after move
+    Q--         Q value: The expectation of the reward of performing action_i when state_i
+    alpha--     learning rate
+    gamma--     future confidence
+    """
+
+    KOMI = -2.5  # Komi BLACK's goal is get higher value, WHITE's goal is get lower value
+
+    def __init__(self, alpha=.7, gamma=.9, init_value=0):
+        self.go = None
+        self.side = None
+        self.alpha = alpha
+        self.gamma = gamma
+        self.init_value = init_value
+        self.q_values = self.init_q_values()  # <board_string, Q[][]>
+
+    def set_go(self, go):
+        self.go = go
+        self.side = go.my_player
+
+    def init_q_values(self):
+        q_values = {}
+
+        if not os.path.exists('QvalueDB.txt'):
+            with open("QvalueDB.txt", 'w') as f:
+                return q_values
+
+        with open("QvalueDB.txt", 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                data = line.split('|')
+                board = data[0]
+                q = np.asarray(list(map(float, data[1:])))
+                q = np.reshape(q, (5, 5))
+                q_values[board] = q
+        return q_values
+
+    def flatten_board(self, board):
+        return [i for row in board for i in row]
+
+    def board_string(self, board):
+        return ''.join(str(x) for x in self.flatten_board(board))
+
+    def board_value(self, board):
+        flatten_board = self.flatten_board(board)
+        board_value = flatten_board.count(1) - flatten_board.count(
+            2) + QLearner.KOMI  # BLACK - WHITE + KOMI means BLACK how better than WHITE
+        return board_value
+
+    def R(self, board_before, board_after):
+        R = self.board_value(board_after) - self.board_value(board_before)
+        return R
+
+    def Q(self, board):
+        board_string = self.board_string(board)
+        if board_string not in self.q_values:
+            # init Q
+            q_val = np.zeros((5, 5))
+            q_val.fill(self.init_value)
+            self.q_values[board_string] = q_val
+
+        # current init Q is 0
+        # what if set 0.5 * total liberty deviation as a part of init Q?
+        return self.q_values[board_string]
+
+    def find_max_action(self):
+        move_list = self.go.move_list
+        shuffle(move_list)
+        max_next_Q = -np.inf
+        max_action = ()
+        cur_board_Q = self.Q(self.go.cur_board)
+        for action in move_list:
+            next_Q = cur_board_Q[action[0]][action[1]]
+            if next_Q > max_next_Q:
+                max_next_Q = next_Q
+                max_action = action
+        return max_action, max_next_Q
+
+    def find_min_action(self):
+        move_list = self.go.move_list
+        shuffle(move_list)
+        min_next_Q = np.inf
+        min_action = ()
+        cur_board_Q = self.Q(self.go.cur_board)
+        for action in move_list:
+            next_Q = cur_board_Q[action[0]][action[1]]
+            if next_Q < min_next_Q:
+                min_next_Q = next_Q
+                min_action = action
+        return min_action, min_next_Q
+
+    def visual(self):
+        """
+        copy from host, remove before submit
+        :return:
+        """
+        board = self.go.cur_board
+        print('-' * len(board) * 2)
+        print(self.side)
+        print('-' * len(board) * 2)
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == 0:
+                    print(' ', end=' ')
+                elif board[i][j] == 1:
+                    print('X', end=' ')
+                else:
+                    print('O', end=' ')
+            print()
+        print('-' * len(board) * 2)
+
+    def get_move(self):
+        if self.side == BLACK:
+            max_action, max_next_Q = self.find_max_action()
+            if max_next_Q < 0:
+                self.visual()
+                return "PASS"
+            return max_action
+        else:
+            min_action, min_next_Q = self.find_min_action()
+            if min_next_Q > 0:
+                self.visual()
+                return "PASS"
+            return min_action
+
+    def learn(self, state_action_list):
+        for state_action_state in state_action_list:
+            board_before, move, board_after = state_action_state
+            q_t = self.Q(board_before)
+            q_t1 = self.Q(board_after)
+            max_q_value = np.max(q_t1)
+
+            q_t[move[0]][move[1]] = (1 - self.alpha) * q_t[move[0]][move[1]] \
+                                  + self.alpha * (self.R(board_before, board_after) + self.gamma * max_q_value)
+
+            self.q_values[self.board_string(board_before)] = q_t
+
+        with open("QvalueDB.txt", 'a') as f:
+            for kv in self.q_values.items():
+                string = kv[0] + '|' + '|'.join(str(x) for x in self.flatten_board(kv[1])) + "\n"
+                f.write(string)
+
+
 def get_result(go):
-    strategy = RandomPlayer(go.move_list)
+    strategy = QLearner(go.move_list)
     result = strategy.get_move()
     return result
 
@@ -193,10 +347,10 @@ def read():
         my_player = int(lines[0])
         last_board = []
         cur_board = []
-        for i in range(1,BOARD_SIZE+1):
+        for i in range(1, BOARD_SIZE + 1):
             cur_line = [int(x) for x in lines[i].strip()]
             last_board.append(cur_line)
-        for i in range(BOARD_SIZE+1, BOARD_SIZE+BOARD_SIZE+1):
+        for i in range(BOARD_SIZE + 1, BOARD_SIZE + BOARD_SIZE + 1):
             cur_line = [int(x) for x in lines[i].strip()]
             cur_board.append(cur_line)
         return my_player, last_board, cur_board
@@ -209,15 +363,11 @@ def write(result):
         else:
             out_result = f'{result[0]},{result[1]}'
             f.write(out_result)
-        f.close()
 
 
 if __name__ == '__main__':
     my_player_, last_board_, cur_board_ = read()
-    # print()
-    # print(Go(my_player_, last_board_, cur_board_).move_list)
-    # print()
+
     go = Go(my_player_, last_board_, cur_board_)
     result = get_result(go)
-    # print(result)
     write(result)
